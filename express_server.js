98fb9e52e7ae52779e82dda86890b068978103f6
Jessie-p05 <jxpei05@gmail.com>
2021-04-13 02:47:25 +0000

    create a post request for form sunmit

--- express_server.js
+++ express_server.js
@@ -3,10 +3,16 @@ const app = express();
 const PORT = 8080; // default port 8080
 app.set('view engine', 'ejs');
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+function generateRandomString() {
+  let r = Math.random().toString(36).substring(7);
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -16,18 +22,26 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: /* What goes here? */ urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
+
+
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);

202e2777cd4f339bcb7b82b4b8e447168163ddb4
Jessie-p05 <jxpei05@gmail.com>
2021-04-13 22:52:26 +0000

    add a delete button by using POST for each url

--- express_server.js
+++ express_server.js
@@ -33,10 +33,9 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.post("/urls", (req, res) => {
@@ -45,6 +44,14 @@ app.post("/urls", (req, res) => {
   const templateVars = { shortURL: randomStr, longURL: /* What goes here? */ req.body.longURL};
   res.render("urls_show", templateVars);         
 });
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL]; 
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars); 
+});
+       
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
